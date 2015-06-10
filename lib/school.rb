bfd3eb8f2971fbd70c0341b560ff7e7f03696508
apradhn <abhijit.pradhan6@gmail.com>
2015-06-10 15:55:59 -0400

    all done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# Write code here
+require "pry"
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.each.with_object({}) {|(grade, names), sorted| sorted[grade] = names.sort}
+  end
+end
